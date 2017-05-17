6e8e2c5b8faeb2615f98af56f6cb7bea3c5b4bfc
Aleksey Dorokhov <akeinhell@gmail.com>
2017-05-17 14:00:32 +0700

    add express

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import S7Bot from './bot';
 import TelegramProvider from './bot/providers/telegram-provider';
 import dotenv from 'dotenv';
+import app from './srv/app';
+import './srv';
 
 dotenv.config();
 S7Bot.use(new TelegramProvider({
