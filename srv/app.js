2d8286bcdbdf2702853c5041926f010e176c6e76
Aleksey Dorokhov <akeinhell@gmail.com>
2017-05-17 16:57:41 +0700

    add express

--- srv/app.js
+++ srv/app.js
@@ -21,7 +21,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(stylus.middleware({
+    src: path.join(__dirname, 'public')
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
