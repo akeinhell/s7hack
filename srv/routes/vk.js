f1168da1cacad5830e409b8e70b9646a06332d9a
Aleksey Dorokhov <akeinhell@gmail.com>
2017-05-18 12:01:00 +0700

    add confirmations

--- srv/routes/vk.js
+++ srv/routes/vk.js
@@ -1,14 +1,22 @@
 let express = require('express');
+import logger from '../../bot/logger';
 import eventBus, {EVENT_VK_MESSAGE} from '../../bot/events';
 let router = express.Router();
 
 
 /* GET users listing. */
 router.post('/callback', function (req, res, next) {
-    if (req.body.type === 'message_new') {
-        eventBus.emit(EVENT_VK_MESSAGE, req.body.object);
+    let type = req.body.type;
+    let response = 'incorrect';
+    switch (type) {
+        case 'message_new':
+            response = 'message_new.ok';
+            eventBus.emit(EVENT_VK_MESSAGE, req.body.object);
+            break;
+        case 'confirmation':
+            response = 'ee87901e';
     }
-    res.send('ok');
+    return res.send(response);
 });
 
 module.exports = router;
