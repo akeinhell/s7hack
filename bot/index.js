95455af145c085b21cb517a4b2e2ea3f91f49af8
Aleksey Dorokhov <akeinhell@gmail.com>
2017-05-18 14:04:10 +0700

    dev

--- bot/index.js
+++ bot/index.js
@@ -1,12 +1,12 @@
-import {EVENT_MESSAGE} from "./events";
-import eventBus from './events';
-import util from 'util';
+import eventBus, {EVENT_MESSAGE} from './events';
 import Searcher from './search';
 import logger from './logger';
 import qs from 'querystring';
+import {getState, INIT_STATE} from './state';
+
 
 const getUrl = (from, to) => {
-    console.log('from', from);
+    logger.info('from', from);
     let params = {
         origin: from.city_rus,
         origin_iata: from.iata_code,
@@ -17,48 +17,61 @@ const getUrl = (from, to) => {
         adults: 1,
         children: 0,
         infants: 0,
-        trip_class:0
+        trip_class: 0
     };
     return `https://search.aviasales.ru/?` + qs.stringify(params);
-}
+};
 
 class S7bot {
     constructor() {
         this.providers = [];
     }
+
     use(provider) {
         this.providers.push(provider);
     }
 
+    response
+
     run() {
         eventBus.on(EVENT_MESSAGE, (message, chat, provider) => {
+            getState(provider.constructor.name, chat)
+              .then(state => {
+                  logger.info('EVENT_MESSAGE state', state);
+                  switch (state.state) {
+                      case INIT_STATE:
+                          provider.reply(chat, 'HELLO MAZAFACA');
+                          break;
+                  }
+              });
+            return;
             Searcher
-                .analize(message)
-                .then(tokens => {
-                    Searcher
-                        .find(tokens)
-                        .then(data => {
-                            const airports = data.filter((i) => i).slice(0, 2)
-                                .map(a => {
-                                    return {
-                                        ...a,
-                                        text: `${a.city_rus}, ${a.country_rus}`
-                                    }
-                                });
-                            if (airports.length == 2){
-                                let [from, dest] = airports;
-                                let url = getUrl(from, dest);
-                                let msg = `Найден маршрут из ${from.text} в ${dest.text}. Подробности ${url}`;
-                                logger.info('response: '+msg);
-                                provider.reply(chat, msg);
-                            } else {
-                                let tplString = 'Из *Города* В *Город*';
-                                provider.reply(chat, `не удалось найти маршрут. Пример запроса "${tplString}"`);
-                            }
-                        })
-                        .catch(e => logger.error(e))
-                    ;
-                });
+              .analize(message)
+              .then(tokens => {
+                  Searcher
+                    .find(tokens)
+                    .then(data => {
+                        const airports = data.filter((i) => i).slice(0, 2)
+                          .map(a => {
+                              return {
+                                  ...a,
+                                  text: `${a.city_rus}, ${a.country_rus}`
+                              };
+                          });
+                        if (airports.length == 2) {
+                            let [from, dest] = airports;
+                            let url = getUrl(from, dest);
+                            let msg = `Найден маршрут из ${from.text} в ${dest.text}. Подробности ${url}`;
+                            logger.info('response: ' + msg);
+                            provider.reply(chat, msg);
+                        } else {
+                            let tplString = 'Из *Города* В *Город*';
+                            provider.reply(chat, `не удалось найти маршрут. Пример запроса "${tplString}"`);
+                        }
+                    })
+                    .catch(e => logger.error(e))
+                  ;
+              });
         });
 
         this.providers.forEach((provider) => {
---
+++ bot/state.js
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+import redisClient from 'redis';
+import bluebird from 'bluebird';
+
+export const INIT_STATE = 'START';
+
+dotenv.config();
+bluebird.promisifyAll(redisClient.RedisClient.prototype);
+bluebird.promisifyAll(redisClient.Multi.prototype);
+
+
+let redis = redisClient.createClient({
+    password: process.env.REDIS_PASS
+});
+
+const getKey = (p, c) => {
+    return `S7:${p}:${c}`
+}
+
+export const saveState = (provider, chat, state) =>  {
+    let key = getKey(provider, chat);
+    redis.set(key, JSON.stringify(state));
+    redis.expire(key, 60*60*3);
+}
+
+function getState(provider, chat) {
+
+    return redis
+      .getAsync(getKey(provider, chat))
+      .then(state => {
+          try{
+              return JSON.parse(state);
+          }catch (e) {
+              return null;
+          }
+      })
+      .then(state => {
+          if (!state) {
+
+              state = {
+                  state: INIT_STATE,
+                  data: {}
+              };
+              saveState(provider, chat, state);
+          }
+          return state;
+      });
+}
+export {
+    getState
+};
+redis.on('error', function (err) {
+    logger.error('RedisError ' + err);
+});
